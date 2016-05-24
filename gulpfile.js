8f668c08568a7362c44c9b7321056ec80880041a
Markus Hoesel <marho348@ad.liu.se>
2016-05-24 13:12:54 +0200

    Configuring gulp to use nodemon, application is now started through gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp');
 
 // Include Our Plugins
 var jshint = require('gulp-jshint');
+var nodemon = require('gulp-nodemon');
 
 // Lint Task
 gulp.task('lint', function() {
@@ -11,5 +12,16 @@ gulp.task('lint', function() {
         .pipe(jshint.reporter('default'));
 });
 
+// Nodemon Task
+gulp.task('start', function() {
+  nodemon({
+    env: { 
+      'NODE_ENV': 'production',
+      'MONGODB_URI': 'mongodb://heroku_49dmvtk4:h5u702lhm1dcfq5bgb7eno7vko@ds011331.mlab.com:11331/heroku_49dmvtk4',
+      'API_KEY': 'AIzaSyCUy8YzvCFHzc8SMvykFhP6WGcMJLZwR-k'
+      }
+  })
+})
+
 // Default Task
-gulp.task('default', ['lint']);
+gulp.task('default', ['lint','start']);
